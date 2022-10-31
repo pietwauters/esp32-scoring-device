2849dc60488a3c65761dc0ec11b34b105ba6c633
pietwauters <67114377+pietwauters@users.noreply.github.com>
2022-10-31 22:42:39 +0100

    Update hardwaredefinition.h
    You now have to uncomment one line. Otherwise the project will not build

--- hardwaredefinition.h
+++ hardwaredefinition.h
@@ -1,8 +1,9 @@
 #ifndef HARDWAREDEFINITIONS_H
 #define HARDWAREDEFINITIONS_H
 
-
+// You have to uncomment only one of the lines below.
+// Without it, the project will not successfully build
 //#define FIRST_PROTO 1
-#define SECOND_PROTO 1
+//#define SECOND_PROTO 1
 
 #endif
