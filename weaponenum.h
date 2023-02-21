6d6a83e4f12a5b0faaca81b5a4622933f1f4bdf0
pietwauters <67114377+pietwauters@users.noreply.github.com>
2023-02-21 15:23:24 +0100

    Update weaponenum.h

--- weaponenum.h
+++ weaponenum.h
@@ -1,5 +1,5 @@
 //Copyright (c) Piet Wauters 2022 <piet.wauters@gmail.com>
 #ifndef WEAPONENUM_H
 #define WEAPONENUM_H
-enum weapon_t {FOIL, EPEE, SABRE, UNKNOWN};
+enum weapon_t {FOIL=0, EPEE, SABRE, UNKNOWN};
 #endif // WEAPONENUM_H
