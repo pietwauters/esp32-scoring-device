5881928516183920e5759097ee7e63c3fab229d7
pietwauters <67114377+pietwauters@users.noreply.github.com>
2023-08-29 15:09:41 +0200

    Update hardwaredefinition.h
    As most people will probably be using the second proto, I now default to SECOND_PROTO.
    It turned out that not doing so, was confusing more than it solved

--- hardwaredefinition.h
+++ hardwaredefinition.h
@@ -4,6 +4,6 @@
 // You have to uncomment only one of the lines below.
 // Without it, the project will not successfully build
 //#define FIRST_PROTO 1
-//#define SECOND_PROTO 1
+#define SECOND_PROTO 1
 
 #endif
