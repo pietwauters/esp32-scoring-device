f6cbc6979d70a9dd5880d32b25cacb5a9f104522
pietwauters <67114377+pietwauters@users.noreply.github.com>
2025-01-17 11:33:53 +0100

    Moved some functionality out of .ino into classes

--- esp32-scoring-device.ino
+++ esp32-scoring-device.ino
@@ -30,9 +30,8 @@
 
 
 
-#define WELCOME_ANIMATION_SPEED 70
 
-WS2812B_LedStrip MyLedStrip;
+//WS2812B_LedStrip &MyLedStrip = WS2812B_LedStrip::getInstance();
 TimeScoreDisplay MyTimeScoreDisplay;
 MultiWeaponSensor MySensor(1);
 NetWork MyNetWork;
@@ -93,211 +92,53 @@ void StateMachineHandler(void *parameter)
   }
 }
 
-TaskHandle_t LedStripTask;
-void LedStripHandler(void *parameter)
-{
-  while(true)
-  {
-    MyLedStrip.ProcessEventsBlocking();
-    esp_task_wdt_reset();
+
+
+
+
+void GoBackToSleepIfWokenByTimer(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  if( ESP_SLEEP_WAKEUP_TIMER == wakeup_reason){
+    delay(300);
+    esp_deep_sleep_start();
   }
-}
-
-void PrintReasonForReset()
-{
-  Serial.print("reason for reset: ");
-  esp_reset_reason_t ResetReason = esp_reset_reason();
-  switch(ResetReason)
-  {
-    case ESP_RST_UNKNOWN:
-    Serial.println("ESP_RST_UNKNOWN");
-    break;
-
-    case ESP_RST_POWERON:
-    Serial.println("ESP_RST_POWERON");
-    break;
-
-    case ESP_RST_EXT:
-    Serial.println("ESP_RST_EXT");
-    MyLedStrip.ClearAll();
-    MyLedStrip.setWhiteLeft(true);
-    MyLedStrip.setWhiteRight(true);
-    MyLedStrip.myShow();
-    delay(5000);
-    break;
-
-    case ESP_RST_SW:
-    Serial.println("ESP_RST_SW");
-    MyLedStrip.ClearAll();
-    MyLedStrip.setWhiteLeft(true);
-    MyLedStrip.setWhiteRight(true);
-    MyLedStrip.myShow();
-    delay(5000);
-    break;
-
-    case ESP_RST_PANIC:
-    Serial.println("ESP_RST_PANIC");
-    MyLedStrip.ClearAll();
-    MyLedStrip.setRed(true);
-    MyLedStrip.myShow();
-    delay(5000);
-    break;
-
-    case ESP_RST_INT_WDT:
-    Serial.println("ESP_RST_INT_WDT");
-    MyLedStrip.ClearAll();
-    MyLedStrip.setRed(true);
-    MyLedStrip.setGreen(true);
-    MyLedStrip.myShow();
-    break;
-
-    case ESP_RST_DEEPSLEEP:
-    Serial.println("ESP_RST_DEEPSLEEP");
-    delay(5000);
-    break;
-
-    case ESP_RST_BROWNOUT:
-    Serial.println("ESP_RST_BROWNOUT");
-    MyLedStrip.ClearAll();
-    MyLedStrip.setGreen(true);
-    MyLedStrip.myShow();
-    delay(5000);
-    break;
-
-    case ESP_RST_SDIO:
-    Serial.println("ESP_RST_SDIO");
-    break;
-
-    case ESP_RST_TASK_WDT:
-    Serial.println("ESP_RST_TASK_WDT");
-    MyLedStrip.ClearAll();
-    MyLedStrip.setWhiteLeft(true);
-    MyLedStrip.myShow();
-    delay(5000);
-    break;
-
-    case ESP_RST_WDT:
-    Serial.println("ESP_RST_WDT");
-    MyLedStrip.ClearAll();
-    MyLedStrip.setWhiteRight(true);
-    MyLedStrip.myShow();
-    delay(5000);
-    break;
-
-  }
-}
-
-void ShowWelcomeLights()
-{
-  for(int i = 0; i< 50; i++)
-  {
-    MyLedStrip.ClearAll();
-    delay(10);
-  }
-
-  MyLedStrip.setWhiteLeft(true);
-  MyLedStrip.myShow();
-  esp_task_wdt_reset();
-  delay(5 * WELCOME_ANIMATION_SPEED);// Block for 500ms.
-  MyLedStrip.setWhiteLeft(false);
-  MyLedStrip.myShow();
-  MyLedStrip.setRed(true);
-  MyLedStrip.myShow();
-  esp_task_wdt_reset();
-  delay(5 * WELCOME_ANIMATION_SPEED);// Block for 500ms.
-  MyLedStrip.setRed(false);
-  MyLedStrip.myShow();
-  MyLedStrip.setWhiteRight(true);
-  MyLedStrip.myShow();
-  esp_task_wdt_reset();
-  delay(5 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.setWhiteRight(false);
-  MyLedStrip.myShow();
-  MyLedStrip.setGreen(true);
-  MyLedStrip.myShow();
-  esp_task_wdt_reset();
-  delay(5 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.ClearAll();
-  MyLedStrip.setUWFTimeLeft(1);
-  MyLedStrip.setUWFTimeRight(1);
-  MyLedStrip.myShow();
-  delay(2 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.setUWFTimeLeft(2);
-  MyLedStrip.setUWFTimeRight(2);
-  MyLedStrip.myShow();
-  delay(2 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.setUWFTimeLeft(3);
-  MyLedStrip.setUWFTimeRight(3);
-  MyLedStrip.myShow();
-  delay(2 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.setUWFTimeLeft(4);
-  MyLedStrip.setUWFTimeRight(4);
-  MyLedStrip.myShow();
-  delay(2 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.setUWFTimeLeft(5);
-  MyLedStrip.setUWFTimeRight(5);
-  MyLedStrip.myShow();
-  delay(2 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.setUWFTimeLeft(6);
-  MyLedStrip.setUWFTimeRight(6);
-  MyLedStrip.myShow();
-  delay(4 * WELCOME_ANIMATION_SPEED);
-  MyLedStrip.setUWFTimeLeft(0);
-  MyLedStrip.setUWFTimeRight(0);
-  MyLedStrip.myShow();
 }
 
 bool bIsRepeater = false;
 void setup() {
 
   MyLedStrip.begin();
+
   // put your setup code here, to run once:
   Serial.begin(115200);
   MyLedStrip.ClearAll();
+  //GoBackToSleepIfWokenByTimer();
   MyTimeScoreDisplay.begin();
   MyLedStrip.ClearAll();
-//
-
-
-
-  //MyTimeScoreDisplay.DisplayWeapon(EPEE);
   MyTimeScoreDisplay.DisplayPisteId();
-  MyLedStrip.ClearAll();
+
   Preferences mypreferences;
   mypreferences.begin("scoringdevice", RO_MODE);
   bIsRepeater = mypreferences.getBool("RepeaterMode",false);
   mypreferences.end();
-  //PrintReasonForReset();    // This is only for debugging instabilities. Comment out when you think it works
+
   MyNetWork.begin();
-  MyLedStrip.ClearAll();
-  ShowWelcomeLights();
+
   MyNetWork.GlobalStartWiFi();
   Serial.println("Wifi started");
   Serial.println("by now the you should have seen all the lights one by one");
 
-  MyLedStrip.ClearAll();
-
-
   MyUDPIOHandler.ConnectToAP();
   MyUDPIOHandler.attach(MyNetWork);
-  MyLedStrip.ClearAll();
-
-
   esp_task_wdt_init(10, false);
-  xTaskCreatePinnedToCore(
-            LedStripHandler,        /* Task function. */
-            "LedStripHandler",      /* String with name of task. */
-            10240,                            /* Stack size in words. */
-            NULL,                            /* Parameter passed as input of the task */
-            0,                                /* Priority of the task. */
-            &LedStripTask,           /* Task handle. */
-            1);
-  esp_task_wdt_add(LedStripTask);
 // In repeater mode don't start these 2 tasks
 if(!bIsRepeater){
 Serial.println("Bwahahaaha I am the master!");
+  MyStatemachine.ResetAll();
+
   MyFPA422Handler.StartWiFi();
-  MyStatemachine.attach(MyLedStrip);
+
   MyStatemachine.attach(MyFPA422Handler);
   MyStatemachine.attach(MyTimeScoreDisplay);
   MyUDPIOHandler.attach(MyStatemachine);
@@ -309,7 +150,7 @@ Serial.println("Bwahahaaha I am the master!");
   xTaskCreatePinnedToCore(
             CoreScoringMachineHandler,        /* Task function. */
             "CoreScoringMachineHandler",      /* String with name of task. */
-            10240,                            /* Stack size in words. */
+            24576,                            /* Stack size in words. */
             NULL,                             /* Parameter passed as input of the task */
             0,                                /* Priority of the task. */
             &CoreScoringMachineTask,           /* Task handle. */
@@ -320,22 +161,21 @@ Serial.println("Bwahahaaha I am the master!");
 
   xTaskCreatePinnedToCore(
             StateMachineHandler,        /* Task function. */
-            "StateMachineHandlerMyLedStrip",      /* String with name of task. */
-            16384,                            /* Stack size in words. 65535*/
+            "StateMachineHandler",      /* String with name of task. */
+            24576,                            /* Stack size in words. 65535*/
             NULL,                             /* Parameter passed as input of the task */
-            0,                                /* Priority of the task. */
+            5,                                /* Priority of the task. */
             &StateMachineTask,           /* Task handle. */
             1);
   esp_task_wdt_add(StateMachineTask);
 
 
   MySensor.begin();
-
-
-  MyLedStrip.ClearAll();
-  delay(100);
-  MyLedStrip.ClearAll();
-  MyStatemachine.ResetAll();
+  StopSearchingForWifi = millis() + 60000;
+  MyCyranoHandler.Begin();
+  MyRepeaterSender.begin();
+  MyStatemachine.attach(MyRepeaterSender);
+  MyStatemachine.attach(MyLedStrip);
   MyStatemachine.SetMachineWeapon(MySensor.GetActualWeapon());
   switch (MySensor.GetActualWeapon()) {
     case FOIL:
@@ -348,11 +188,6 @@ Serial.println("Bwahahaaha I am the master!");
     MyStatemachine.StateChanged(EVENT_WEAPON | WEAPON_MASK_SABRE);
     break;
   }
-
-  StopSearchingForWifi = millis() + 60000;
-  MyCyranoHandler.Begin();
-  MyRepeaterSender.begin();
-  MyStatemachine.attach(MyRepeaterSender);
 }
 else{
   // When running in repeater mode
@@ -370,26 +205,33 @@ else{
 void loop() {
   // put your main code here, to run repeatedly:
   delay(1); // without this it simply doesn't work
+  //vTaskDelay(1 / portTICK_PERIOD_MS);
   if(!bIsRepeater){
     MyFPA422Handler.WifiPeriodicalUpdate();
-    yield();
+    esp_task_wdt_reset();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
     MyTimeScoreDisplay.ProcessEvents();
-    yield();
+    esp_task_wdt_reset();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
     MyFPA422Handler.WifiPeriodicalUpdate();
-    yield();
+    esp_task_wdt_reset();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
     if(MyNetWork.IsExternalWifiAvailable())
     {
       MyCyranoHandler.PeriodicallyBroadcastStatus();
-      yield();
+      esp_task_wdt_reset();
+      vTaskDelay(1 / portTICK_PERIOD_MS);
       MyCyranoHandler.CheckConnection();
       MyFPA422Handler.WifiPeriodicalUpdate();  // Not really needed because already done above
     }
-    yield();
+    esp_task_wdt_reset();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
 
     if(MyStatemachine.IsConnectedToRemote())
     {
       MyTimeScoreDisplay.CycleScoreMatchAndTimeWhenNotFighting();
-      yield();
+      esp_task_wdt_reset();
+      vTaskDelay(1 / portTICK_PERIOD_MS);
       MyFPA422Handler.WifiPeriodicalUpdate();
       MyLedStrip.AnimatePrio();
       MyFPA422Handler.WifiPeriodicalUpdate();
@@ -397,15 +239,24 @@ void loop() {
     }
     MyLedStrip.AnimateWarning();
     MyStatemachine.PeriodicallyBroadcastFullState(&MyRepeaterSender,FULL_STATUS_REPETITION_PERIOD);
-    yield();
+    esp_task_wdt_reset();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
     MyRepeaterSender.RepeatLastMessage();
-    yield();
+    esp_task_wdt_reset();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
     //MyRepeaterSender.BroadcastHeartBeat();
+    if(MyStatemachine.GoToSleep())
+    {
+      vTaskSuspend(CoreScoringMachineTask);
+      delay(100);
+      prepareforDeepSleep();
+    }
   }
   else{ // when in repeater mode
     MyTimeScoreDisplay.ProcessEvents();
     MyTimeScoreDisplay.CycleScoreMatchAndTimeWhenNotFighting();
-    yield();
+    esp_task_wdt_reset();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
     MyLedStrip.AnimatePrio();
     MyLedStrip.AnimateWarning();
     if(MyRepeaterReiver.IsWatchDogTriggered())
